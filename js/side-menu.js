d2d4b3b2081d7b66fb8e97de88f953c0479f03e4
jonathanFranco <jonathanfranco01@outlook.com>
2019-08-11 18:01:46 -0300

    edit navbar

--- js/side-menu.js
+++ js/side-menu.js
@@ -13,14 +13,15 @@ function closeNav() {
 //Navbar Animation
  $(window).on('scroll', function () {
       if ($(window).scrollTop()) {
-        $("#logo-span,#logo-span2").css({ 'font-size': '30px' });
-        $("#logo-span,#logo-span2").css({ 'transition': '0.1s' });
+        $("#logo-span,#logo-span2").css({ 'font-size': '30px','transition': '0.1s' });
+        $("#logo-span,#logo-span2").css({ 'color': '#fff' });
         $(".nav-effect").css({ 'transform': 'scale(0.9)' });
         $(".nav-effect").css({ 'transition': '0.3s' });
         $(".navbar").css({ 'box-shadow': '2px 2px 2px black' });
       } else {
-        $("#logo-span,#logo-span2").css({ 'font-size': '40px' });
-        $("#logo-span,#logo-span2").css({ 'transition': '0.1s' });
+        $("#logo-span,#logo-span2").css({ 'font-size': '40px','transition': '0.1s' });
+        $("#logo-span").css({ 'color': '#33b5e5' });
+        $("#logo-span2").css({ 'color': '#4285F4' });
         $(".nav-effect").css({ 'transform': 'scale(1.0)' });
         $(".nav-effect").css({ 'transition': '0.3s' });
         $(".navbar").css({ 'box-shadow': '2px 2px 2px black' });
