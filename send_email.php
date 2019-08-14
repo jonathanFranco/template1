8f3faa00518ce1016a7993c5abd6019a977c7ae3
jonathanFranco <jonathanfranco01@outlook.com>
2019-08-14 02:59:17 -0300

    home intro edit

--- send_email.php
+++ send_email.php
@@ -4,7 +4,7 @@
   <meta charset="utf-8" name="viewport"
     content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
   <meta author="Jonathan Franco">
-  <title>Enviando Email!</title>
+  <title>Email Enviado!</title>
   <link rel="icon" href="img/icon.png" type="image/png" />
   <link rel="stylesheet" type="text/css" href="css/bootstrap.css">
   <link rel="stylesheet" type="text/css" href="css/style.css">
@@ -31,14 +31,14 @@
       <h3 class="text-light"><i class="fas fa-envelope"></i> Concluído</h3>
       <h3 class="card-title pt-2"><strong>Seu Email Foi Enviado Com Sucesso!!!</strong></h3>
       <h5>Nós retornaremos o contato em breve, sua pergunta será respondida o mais rápido possível.</h5>
-     <a class="btn btn-light my-5" href="contato.html"><i class="fas fa-arrow-left"></i> Voltar</a>
+     <a class="btn btn-light btn-rounded my-5" href="contato.html"><i class="fas fa-arrow-left"></i> Voltar</a>
     </div>
   </div>
 
 </div>
 <!-- Card -->
   </div>
-  
+
     <script>
     $(window).load(function(){
       <?php
@@ -51,14 +51,14 @@
          $mail->Port = 465;
          $mail->SMTPSecure = 'ssl';
          $mail->SMTPAutoTLS = false;
-         $mail->Username = 'email@gmail.com'; // Conta de email existente e ativa em seu domínio
-         $mail->Password = 'senha'; // Senha da sua conta de email
+         $mail->Username = 'luffyopelric@gmail.com'; // Conta de email existente e ativa em seu domínio
+         $mail->Password = '87287051'; // Senha da sua conta de email
          // DADOS DO REMETENTE
-         $mail->Sender = "email@gmail.com"; // Conta de email existente e ativa em seu domínio
-         $mail->From = "email@gmail.com"; // Sua conta de email que será remetente da mensagem
+         $mail->Sender = "luffyopelric@gmail.com"; // Conta de email existente e ativa em seu domínio
+         $mail->From = "luffyopelric@gmail.com"; // Sua conta de email que será remetente da mensagem
          $mail->FromName = "Contato ACESHAC"; // Nome da conta de email
          // DADOS DO DESTINATÁRIO
-         $mail->AddAddress('email@gmail.com'); // Define qual conta de email receberá a mensagem
+         $mail->AddAddress('luffyopelric@gmail.com'); // Define qual conta de email receberá a mensagem
          $mail->IsHTML(true);
          $mail->CharSet = 'utf-8';
          // DEFINIÇÃO DA MENSAGEM
@@ -67,8 +67,6 @@
          $mail->Body .= " Nome:".$_POST['nome']."<br />"."";
          // Texto da mensagem
          $mail->Body .= " E-mail: ".$_POST['email']."<br />"."";
-         // Texto da mensagem
-         $mail->Body .= " Telefone: ".$_POST['tel']."<br />"."";
          // Texto da mensagem
          $mail->Body .= " Mensagem: ".$_POST['msg']."<br />"."";
          // ENVIO DO EMAIL
