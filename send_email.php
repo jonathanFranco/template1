2e0c5361677d9ac5f96c110e6097a10f52adf4e1
jonathanFranco <jonathanfranco01@outlook.com>
2019-08-19 17:15:23 -0300

    edit site

--- send_email.php
+++ send_email.php
@@ -6,11 +6,11 @@
   <meta author="Jonathan Franco">
   <title>Email Enviado!</title>
   <link rel="icon" href="img/icon.png" type="image/png" />
-  <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+  <link rel="stylesheet" type="text/css" href="css/bootstrap.css">
   <link rel="stylesheet" type="text/css" href="css/style.css">
-  <link href="https://fonts.googleapis.com/css?family=Quicksand:300&display=swap" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css?family=Roboto&display=swap" rel="stylesheet">
   <link rel="stylesheet" type="text/css" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css">
-  <script type="text/javascript" src="js/bootstrap.min.js"></script>
+  <script type="text/javascript" src="js/bootstrap.js"></script>
   <script type="text/javascript" src="js/jquery.min.js"></script>
   <link rel="stylesheet" type="text/css" href="css/mdb.css">
   <script type="text/javascript" src="js/mdb.js"></script>
@@ -39,7 +39,7 @@
 <!-- Card -->
   </div>
 
-<script>
+    <script>
     $(window).load(function(){
       <?php
          require_once("phpmailer/PHPMailerAutoload.php");
