fcccaa50cda1399dc636d3c2e99d98780427ae00
jonathanFranco <jonathanfranco01@outlook.com>
2019-09-04 14:00:42 -0300

    edit site

--- js/side-menu.js
+++ js/side-menu.js
@@ -1,25 +1,26 @@
 //Mobile Menu Script
 function openNav() {
-  document.getElementById("myNav").style.width = "100%";
+  document.getElementById("myNav").style.width = "85%";
   document.getElementById("menu-content").style.display = "initial";
-  $("#side-menu-nav").css({'display':'block'});
+  $("#side-menu-nav,#hr-mobile").css({'display':'block'});
 }
 
 function closeNav() {
   document.getElementById("myNav").style.width = "0%";
   document.getElementById("menu-content").style.display = "none";
-  $("#side-menu-nav").css({'display':'none'});
+  $("#side-menu-nav,#hr-mobile").css({'display':'none'});
 }
 
 //Navbar Animation
  $(window).on('scroll', function () {
       if ($(window).scrollTop()) {
         $("#logo").css({ 'transform': 'scale(0.8)','transition':'0.2s'});
-        $(".nav-effect").css({ 'transform': 'scale(0.9)' });
+        $(".nav-effect").css({ 'transform': 'scale(0.9)' , 'color': '#fff'});
         $(".nav-effect").css({ 'transition': '0.3s' });
       } else {
         $("#logo").css({ 'transform': 'scale(1.0)','transition':'0.2s'});
         $(".nav-effect").css({ 'transform': 'scale(1.0)' });
         $(".nav-effect").css({ 'transition': '0.3s' });
       }
+
     });
